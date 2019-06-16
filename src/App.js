dca6f2ee0038238eb459fd4ddc189f6e9d8e3bd1
Yucong Lei <yuconglei@Yucongs-MacBook-Pro.local>
2019-06-16 13:50:26 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import config from './config'
+import {Button, Form} from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      wxweatherData: {},
+      city: ''
+
+    }
+this.onUpdateCity = this.onUpdateCity.bind(this)
+this.getWeather = this.getWeather.bind(this)
+this.submit = this.submit.bind(this)
+  }
+
+  getWeather(city, unit){
+    var self =this
+    var cityName = city || 'chengdu'
+    var unityType = unit || 'metric'
+    axios({
+      method: 'get',
+      url: 'http://api.openweathermap.org/data/2.5/forecast',
+      params: {
+        q:cityName,
+        units: unityType,
+        appid: config.appid
+
+      }
+    })
+    .then(function(res){
+      const data = res.data
+      self.setState({wxweatherData: data})
+      console.log(res)
+    })
+    .catch(function(err){
+      console.log(err)
+
+    })
+
+  }
+
+  componentDidMount(){
+    this.getWeather('boston', 'metric')
+    
+  }
+  onUpdateCity(ev){
+console.log(ev.target.value)
+this.setState({city: ev.target.value})
+
+
+  }
+  submit(){
+    this.getWeather(this.state.city)
+  }
+  render(){
+    console.log('this is render')
+    console.log(this.props)
+    return<Form>
+      <Form.Field>
+        <label>Enter CityName</label>
+        <input placeholder = 'city name' value={this.state.city} onChange={this.onUpdateCity} type='test'/>
+      </Form.Field>
+      <Button onClick={this.submit}>Submit</Button>
+    </Form>
+    // <div>weather<input value={this.state.city} onChange={this.onUpdateCity} type='text'/>
+    // <button onClick={this.submit}>Submit</button>
+    // </div>
+
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    appid: 'f5a736ea890e6b1ec5181abd1f0f8a1f',
+    units: 'metric',
+    city: 'seattle'
+}
+export default config
